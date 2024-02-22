8a4a46796d351dd18ea90270c9361cd346b77692
Reazy96 <resul.sadriu@outlook.com>
2024-02-22 19:58:44 +0100

    update

--- main.js
+++ main.js
@@ -7,37 +7,54 @@ let sunsetOutput = document.querySelector(".sunset");
 let conditionOutput = document.querySelector(".condition");
 let tempOutput = document.querySelector(".temp");
 let timeOutput = document.querySelector(".time");
+let countryOutput = document.querySelector(".country");
+let iconOutput = document.querySelector(".icon");
 
 let checkBtn = document.querySelector(".btn");
 
 checkBtn.addEventListener("click", () => {
   let cityInput = document.querySelector(".city-input").value;
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityInput},lat=52.520008&lon=13.404954&appid=9402f793619e51d05bb385d059512f3c&units=metric&lang=de`
+    `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=50&appid=9402f793619e51d05bb385d059512f3c`
   )
     .then((resp) => resp.json())
     .then((data) => {
-      console.log(data);
-      let sunriseMs = data.sys.sunrise;
-      console.log(sunriseMs);
-      let date1 = new Date(sunriseMs);
-      console.log(date1);
-      let sunrise = date1.toLocaleTimeString();
-      console.log(sunrise);
+      let lat = data[0].lat;
+      let lon = data[0].lon;
 
-      let sunsetMs = data.sys.sunset;
-      console.log(sunsetMs);
-      let date2 = new Date(sunsetMs);
-      console.log(date2);
-      let sunset = date2.toLocaleTimeString();
-      console.log(sunrise);
+      console.log(lat);
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9402f793619e51d05bb385d059512f3c&units=metric&lang=de`
+      )
+        .then((respo) => respo.json())
+        .then((singledata) => {
+          let sunriseMs = singledata.sys.sunrise;
 
-      tempOutput.innerHTML = `<p>${data.main.temp}</p> <p>°C</p>`;
-      conditionOutput.innerHTML = `<p>${data.weather[0].description}</p>`;
-      windOutput.innerHTML = `<h3>Windgeschwindigkeit:</h3> <p>${data.wind.speed}</p>`;
-      pressureOutput.innerHTML = `<h3>atmosphärischer Druck:</h3> <p>${data.main.pressure}</p>`;
-      humidityOutput.innerHTML = `<h3>Luftfeuchtigkeit:</h3> <p>${data.main.humidity}</p>`;
-      sunriseOutput.innerHTML = `<h3>Sonnenaufgang</h3> <p>${sunrise}</p>`;
-      sunsetOutput.innerHTML = `<h3>Sonnenuntergang</h3> <p>${sunset}</p>`;
+          let date1 = new Date(sunriseMs);
+
+          let sunrise = date1.toLocaleTimeString();
+          // -------------------------------------------------------------
+          let sunsetMs = singledata.sys.sunset;
+
+          let date2 = new Date(sunsetMs);
+
+          let sunset = date2.toLocaleTimeString();
+
+          console.log(singledata.sys.country);
+
+          let icons = `https://openweathermap.org/img/wn/`;
+
+          console.log(singledata);
+
+          countryOutput.innerHTML = `<p> ${singledata.sys.country}</p>`;
+          iconOutput.innerHTML = `<img src="${icons}${singledata.weather[0].icon}@2x.png"/>`;
+          tempOutput.innerHTML = `<p>${singledata.main.temp}</p> <p>°C</p>`;
+          conditionOutput.innerHTML = `<p>${singledata.weather[0].description}</p>`;
+          windOutput.innerHTML = `<h3>Windgeschwindigkeit:</h3> <p>${singledata.wind.speed}</p>`;
+          pressureOutput.innerHTML = `<h3>atmosphärischer Druck:</h3> <p>${singledata.main.pressure}</p>`;
+          humidityOutput.innerHTML = `<h3>Luftfeuchtigkeit:</h3> <p>${singledata.main.humidity}</p>`;
+          sunriseOutput.innerHTML = `<h3>Sonnenaufgang</h3> <p>${sunrise}</p>`;
+          sunsetOutput.innerHTML = `<h3>Sonnenuntergang</h3> <p>${sunset}</p>`;
+        });
     });
 });
