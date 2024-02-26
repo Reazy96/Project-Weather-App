dda527dc55ea47691e12b075c2a043b91b4214da
Reazy96 <resul.sadriu@outlook.com>
2024-02-26 09:09:06 +0100

    done

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let windOutput = document.querySelector(".wind");
-let cloudOutput = document.querySelector(".cloud");
-let pressureOutput = document.querySelector(".pressure");
+
 let humidityOutput = document.querySelector(".humidity");
 let sunriseOutput = document.querySelector(".sunrise");
 let sunsetOutput = document.querySelector(".sunset");
@@ -22,39 +21,49 @@ checkBtn.addEventListener("click", () => {
       let lat = data[0].lat;
       let lon = data[0].lon;
 
-      console.log(lat);
       fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9402f793619e51d05bb385d059512f3c&units=metric&lang=de`
       )
         .then((respo) => respo.json())
         .then((singledata) => {
-          let sunriseMs = singledata.sys.sunrise;
+          let sunriseMs = (singledata.sys.sunrise + singledata.timezone) * 1000;
 
           let date1 = new Date(sunriseMs);
 
           let sunrise = date1.toLocaleTimeString();
           // -------------------------------------------------------------
-          let sunsetMs = singledata.sys.sunset;
+          let sunsetMs = (singledata.sys.sunset + singledata.timezone) * 1000;
 
           let date2 = new Date(sunsetMs);
 
           let sunset = date2.toLocaleTimeString();
 
-          console.log(singledata.sys.country);
+          //  --------------------------------------------------------------
+
+          let actualDate = new Date();
+          let options = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
+          let actualDateFormat = actualDate.toLocaleDateString("de-DE", options);
 
           let icons = `https://openweathermap.org/img/wn/`;
 
-          console.log(singledata);
-
-          countryOutput.innerHTML = `<p> ${singledata.sys.country}</p>`;
           iconOutput.innerHTML = `<img src="${icons}${singledata.weather[0].icon}@2x.png"/>`;
-          tempOutput.innerHTML = `<p>${singledata.main.temp}</p> <p>°C</p>`;
+
           conditionOutput.innerHTML = `<p>${singledata.weather[0].description}</p>`;
-          windOutput.innerHTML = `<h3>Windgeschwindigkeit:</h3> <p>${singledata.wind.speed}</p>`;
-          pressureOutput.innerHTML = `<h3>atmosphärischer Druck:</h3> <p>${singledata.main.pressure}</p>`;
-          humidityOutput.innerHTML = `<h3>Luftfeuchtigkeit:</h3> <p>${singledata.main.humidity}</p>`;
-          sunriseOutput.innerHTML = `<h3>Sonnenaufgang</h3> <p>${sunrise}</p>`;
-          sunsetOutput.innerHTML = `<h3>Sonnenuntergang</h3> <p>${sunset}</p>`;
+          tempOutput.innerHTML = `<h2>${singledata.main.temp}°C</h2> `;
+          timeOutput.innerHTML = `<p>${actualDateFormat}</p>`;
+          countryOutput.innerHTML = `<h4>${cityInput.toUpperCase()}</h4>  <p> ( ${singledata.sys.country} )</p>`;
+
+          windOutput.innerHTML = `<h3>Windgeschwindigkeit</h3> <p>${singledata.wind.speed} km/h</p>`;
+
+          humidityOutput.innerHTML = `<h3>Luftfeuchtigkeit</h3> <p>${singledata.main.humidity} %</p>`;
+          sunriseOutput.innerHTML = `<h3>Sonnenaufgang</h3> <p>${sunrise} Uhr</p>`;
+          sunsetOutput.innerHTML = `<h3>Sonnenuntergang</h3> <p>${sunset} Uhr</p>`;
+        })
+        .catch((error) => {
+          console.error("Error fetching weather data:", error);
         });
+    })
+    .catch((error) => {
+      console.error("Error fetching city data:", error);
     });
 });
